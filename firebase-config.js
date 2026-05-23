7299bf49a9f4baa152c446cd79f54d12cd43b6f1
jani681 <jani681@gmail.com>
2026-05-23 20:23:28 +0500

    Update Firebase configuration details
    Updated Firebase configuration with new project credentials.

--- firebase-config.js
+++ firebase-config.js
@@ -1,16 +1,14 @@
-// firebase-config.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_PROJECT_ID.appspot.com",
-    messagingSenderId: "YOUR_SENDER_ID",
-    appId: "YOUR_APP_ID"
+    apiKey: "AIzaSyBuYYn6V3jqUs47fnVfW1OlCywfyDX6ix0",
+    authDomain: "gatepass-saas.firebaseapp.com",
+    projectId: "gatepass-saas",
+    storageBucket: "gatepass-saas.appspot.com",
+    messagingSenderId: "788886920648",
+    appId: "1:788886920648:web:acf750800bf8a5b0b4f4fd"
 };
 
-// Initialize only once
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
