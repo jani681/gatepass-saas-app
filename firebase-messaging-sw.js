7df0f551efe4c85477e79ccf3f2e70398c60b9fe
jani681 <jani681@gmail.com>
2026-05-24 12:59:59 +0500

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,25 +1,22 @@
-importScripts('https://www.gstatic.com/firebasejs/10.12.0/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/10.12.0/firebase-messaging-compat.js');
+// firebase-messaging-sw.js
 
-// Firebase App Config
+importScripts("https://www.gstatic.com/firebasejs/10.12.0/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/10.12.0/firebase-messaging-compat.js");
+
 firebase.initializeApp({
-    apiKey: "AIzaSyBuYYn6V3jqUs47fnVfW1OlCywfyDX6ix0",
-    projectId: "gatepass-saas",
-    messagingSenderId: "788886920648",
-    appId: "1:788886920648:web:acf750800bf8a5b0b4f4fd"
+  apiKey: "AIzaSyBuYYn6V3jqUs47fnVFvW1OlCywfyDX6ix0",
+  authDomain: "gatepass-saas.firebaseapp.com",
+  projectId: "gatepass-saas",
+  storageBucket: "gatepass-saas.appspot.com",
+  messagingSenderId: "788886920648",
+  appId: "1:788886920648:web:acf750800bf8a5b0b4f4fd"
 });
 
 const messaging = firebase.messaging();
 
-// Background notification handle karne ke liye
 messaging.onBackgroundMessage((payload) => {
-    console.log('[firebase-messaging-sw.js] Received background message ', payload);
-    
-    const notificationTitle = payload.notification.title;
-    const notificationOptions = {
-        body: payload.notification.body,
-        icon: '/logo.png' 
-    };
-
-    self.registration.showNotification(notificationTitle, notificationOptions);
+  self.registration.showNotification("Gate Alert!", {
+    body: payload?.notification?.body || "New guest entry captured",
+    icon: "https://cdn-icons-png.flaticon.com/512/6195/6195700.png"
+  });
 });
